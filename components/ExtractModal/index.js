d48d569c48d877097b02af1ecf2f455e71d8cb86
Jean Flores <jeansfloresof@gmail.com>
2022-02-28 12:41:26 -0300

    chora: update extract modal

--- components/ExtractModal/index.js
+++ components/ExtractModal/index.js
@@ -48,12 +48,19 @@ export const ExtracModal = ({ id }) => {
             </button>
           </div>
 
-          <div className="mockup-code text-white">
-            <pre><code id="extract-code" dangerouslySetInnerHTML={{ __html: buildExtract({ operation: id.toLocaleUpperCase(), ...extract }) }}></code></pre>
-          </div>
-
-          <div className="modal-action">
-            <label className="btn" onClick={() => handleClipBoard()}>Copiar Extrato</label>
+          <div className="bg-base-200 rounded-2xl text-white overflow-x-auto relative">
+            <div data-tip="Copiar Extrato" className="absolute right-2 top-2 tooltip tooltip-left tooltip-primary">
+              <button className="btn btn-square btn-sm" onClick={() => handleClipBoard()}>
+                <svg className="w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                  <path d="M 16 3 C 14.742188 3 13.847656 3.890625 13.40625 5 L 6 5 L 6 28 L 26 28 L 26 5 L 18.59375 5 C 18.152344 3.890625 17.257813 3 16 3 Z M 16 5 C 16.554688 5 17 5.445313 17 6 L 17 7 L 20 7 L 20 9 L 12 9 L 12 7 L 15 7 L 15 6 C 15 5.445313 15.445313 5 16 5 Z M 8 7 L 10 7 L 10 11 L 22 11 L 22 7 L 24 7 L 24 26 L 8 26 Z">
+                  </path>
+                </svg>
+              </button>
+            </div>
+            <pre>
+              <code id="extract-code" dangerouslySetInnerHTML={{ __html: buildExtract({ operation: id.toLocaleUpperCase(), ...extract }) }}>
+              </code>
+            </pre>
           </div>
         </div>
       </div>
--- utils/formatExtract.js
+++ utils/formatExtract.js
@@ -20,27 +20,26 @@ export const buildExtract = (data) => {
     total
   } = data
 
-  return `
-  -----------------------------------------
-              Detalhes da ${operation}
-  -----------------------------------------
-  Vendedor: ${seller}
-  Comprador: ${purchaser}
-
-  -----------------------------------------
-  Descrição dos Itens:
-  -----------------------------------------
-  1. Papelão       ${cardboard.quantity.toString().padStart(10, ' ')}x   $ ${cardboard.value.toFixed(2)}
-  2. Garrafa       ${bottle.quantity.toString().padStart(10, ' ')}x   $ ${bottle.value.toFixed(2)}
-  3. Vidro         ${glass.quantity.toString().padStart(10, ' ')}x   $ ${glass.value.toFixed(2)}
-  4. Latinha       ${tin.quantity.toString().padStart(10, ' ')}x   $ ${tin.value.toFixed(2)}
-  5. Fio           ${wire.quantity.toString().padStart(10, ' ')}x   $ ${wire.value.toFixed(2)}
-  6. Plástico Duro ${rigidPlastic.quantity.toString().padStart(10, ' ')}x   $ ${rigidPlastic.value.toFixed(2)}
-  7. Circuito      ${circuit.quantity.toString().padStart(10, ' ')}x   $ ${circuit.value.toFixed(2)}
-  8. Antena        ${antenna.quantity.toString().padStart(10, ' ')}x   $ ${antenna.value.toFixed(2)}
-  -----------------------------------------
-  Valor Total:                   $ ${total.toFixed(2)}
-  -----------------------------------------
-  Realizada em ${moment().format('LL')}
-  `
+  return (
+    `
+    -----------------------------------------
+               Detalhes da ${operation}
+    -----------------------------------------
+    Vendedor: ${seller}
+    Comprador: ${purchaser}
+    -----------------------------------------
+    1. Papelão       ${cardboard.quantity.toString().padStart(10, ' ')}x   $ ${cardboard.value.toFixed(2)}
+    2. Garrafa       ${bottle.quantity.toString().padStart(10, ' ')}x   $ ${bottle.value.toFixed(2)}
+    3. Vidro         ${glass.quantity.toString().padStart(10, ' ')}x   $ ${glass.value.toFixed(2)}
+    4. Latinha       ${tin.quantity.toString().padStart(10, ' ')}x   $ ${tin.value.toFixed(2)}
+    5. Fio           ${wire.quantity.toString().padStart(10, ' ')}x   $ ${wire.value.toFixed(2)}
+    6. Plástico Duro ${rigidPlastic.quantity.toString().padStart(10, ' ')}x   $ ${rigidPlastic.value.toFixed(2)}
+    7. Circuito      ${circuit.quantity.toString().padStart(10, ' ')}x   $ ${circuit.value.toFixed(2)}
+    8. Antena        ${antenna.quantity.toString().padStart(10, ' ')}x   $ ${antenna.value.toFixed(2)}
+    -----------------------------------------
+    Valor Total:                   $ ${total.toFixed(2)}
+    -----------------------------------------
+    Realizada em ${moment().format('lll')}
+    `
+  )
 }
